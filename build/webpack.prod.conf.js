ed18d36c8d1cd6a723ef4c1f235d0b682d9769e4
TheSimonLam <thesimonlam@gmail.com>
2018-03-24 02:12:16 +0000

    woo

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -119,15 +119,6 @@ const webpackConfig = merge(baseWebpackConfig, {
     //   children: true,
     //   minChunks: 3
     // }),
-
-    // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.build.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ])
   ]
 })
 
