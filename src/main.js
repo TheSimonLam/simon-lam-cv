22c0678c3e398b322bd7df4d27ebdf3513ba2e32
TheSimonLam <thesimonlam@gmail.com>
2018-03-26 21:40:51 +0100

    yo

--- src/main.js
+++ src/main.js
@@ -4,6 +4,30 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+require('./assets/skills/vue.png');
+require('./assets/skills/angular.png');
+require('./assets/skills/webpack.png');
+require('./assets/skills/react.png');
+require('./assets/skills/node.png');
+require('./assets/skills/js.png');
+require('./assets/skills/es6.png');
+require('./assets/skills/yarn.png');
+require('./assets/skills/jquery.png');
+require('./assets/skills/java.png');
+require('./assets/skills/gradle.png');
+require('./assets/skills/maven.png');
+require('./assets/skills/sass.png');
+require('./assets/skills/sonar.png');
+require('./assets/skills/git.png');
+require('./assets/skills/DFP.png');
+require('./assets/skills/design.png');
+require('./assets/skills/affiliate.png');
+require('./assets/skills/music.png');
+require('./assets/skills/video.png');
+require('./assets/skills/game.png');
+require('./assets/skills/ga.png');
+require('./assets/skills/testing.png');
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
